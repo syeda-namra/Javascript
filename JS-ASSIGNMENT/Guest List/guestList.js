3fead51975247b9570b4deecdcd2d5d77c4dbe29
syeda-namra <namra.cyed@gmail.com>
2022-10-21 19:14:21 +0500

    21/10/2022

--- JS-ASSIGNMENT/Guest List/guestList.js
+++ JS-ASSIGNMENT/Guest List/guestList.js
@@ -1,6 +1,8 @@
-let living = [`Anum`, `Aisha`, `Komal`, `Tooba`];
+let guestList = [`Anum`, `Aisha`, `Komal`, `Tooba`];
 
-for(i=0; i < living.length; i++)
+for(i=0; i < guestList.length; i++)
 {
-    console.log(`Hey`,living[i]+`! you're invited for a dinner tonight.\nSee ya soon buddy! :)\n\n`);
+    console.log(`Hey`,guestList[i]+`! you're invited for a dinner tonight.\nSee ya soon buddy! :)\n\n`);
 }
+
+export {guestList};
--- JS-ASSIGNMENT/Intentional Error/intentionalError.js
+++ JS-ASSIGNMENT/Intentional Error/intentionalError.js
@@ -1,8 +1,16 @@
 let arr=["French","France","Himaliya","Juices","Black Sea"];
 
-console.log(arr);
+//console.log(arr);
 
+for(i=0; i < arr.length; i++){
+    if(typeof(arr[i]) == "string"){
+        console.log(arr[i]);
+    }
+    else{
+        console.log(`Intentional Error`);
+    }
+}
 //Intentional Error
 //arr[2]="x","y";
 
-console.log(arr);
+//console.log(arr);
