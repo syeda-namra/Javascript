f0a78d8e1ef29a1983a851ec9504d6350f69b17a
syeda-namra <namra.cyed@gmail.com>
2022-10-26 21:58:30 +0500

    Update cars.js

--- JS-ASSIGNMENT/Cars/cars.js
+++ JS-ASSIGNMENT/Cars/cars.js
@@ -1,3 +1,30 @@
-function make_cars(manufacturer,modelNo, arguments){
+// function make_cars(manufacturer,modelNo, arguments){
     
+// }
+// function makeCar(manufacturer, model_name, color = "blue", two_package = false) {
+//     car = { "Manufacturer": manufacturer, "Model Name": model_name, "Color": color, "Two Package": two_package }
+//     return car
+// }
+
+function makeCar(manufacturer, model, ...options) {
+
+    car_parts = {
+        "manfacturer": manufacturer,
+        "model": model,
+        "moreOptions": options
+    }
+    return car_parts;
 }
+car = makeCar("subaru", "outback", "blue", true)
+console.log(car)
+
+
+car = makeCar("subaru", "outback", "Red")
+console.log(car)
+
+
+car = makeCar("subaru", "outback")
+console.log(car)
+
+car = makeCar("xyz","abc","lmn","pqr","sty","uvw");
+console.log(car)
