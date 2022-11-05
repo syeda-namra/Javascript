b9423d91ea2614d81cf800ebd9927500e177ed3b
syeda-namra <namra.cyed@gmail.com>
2022-11-05 20:53:53 +0500

    Leap Year
    05/11/2022

---
+++ JS-ASSIGNMENT/Leap Year/LeapYear.js
@@ -0,0 +1,40 @@
+let year = 1900;
+
+//console.log (typeof year)
+
+for(i=0;i<150;i++){
+    year++;
+    if( year%4 === 0){
+
+        console.log("Leap Year:",year)
+    }
+    // year=year+3;
+
+    // console.log(year)
+    }
+
+// ( function (){
+
+//     for(i=0;i<1500;i++){
+
+//         year++;
+
+//         if( year%4 === 0 ){
+
+//             console.log('Leap Year:',year);
+//         }
+//     }
+    
+// })();
+
+// (function () {
+//   for(i=0;i<150;i++){
+
+//         year++;
+
+//         if( year%4 === 0 ){
+            
+//             console.log('Leap Year:',year,'\n');
+//         }
+//     }
+// })();
